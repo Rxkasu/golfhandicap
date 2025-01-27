702d19e7083b7c94013824fb16880099298c3b37
Philipp Arnold <20288996+SkyllarRisen@users.noreply.github.com>
2025-01-27 18:39:15 +0100

    chore: added some docu for running tests

---
+++ tests/readme.md
@@ -0,0 +1,10 @@
+To run the tests install vitest as a dependency using:
+
+    npm i -D vitest #might not be necessary as files are already in git
+
+Then run the tests using:
+
+    npm test
+
+The framework looks for all files ending in *.test.js.
+
--- tests/template.test.js
+++ tests/template.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { template } from "../template";
+import { template } from "../src/template";
 
 describe('TEMPLATE', () => {
     it('TEST_NAME_1', () => {
@@ -18,5 +18,7 @@ describe('TEMPLATE', () => {
     it('TEST_NAME_2', () => {
         // Alternative:
         expect(template(1, -2)).toBe(1);
+        expect(template(-2, 1)).toBe(1);
+        expect(template(1, 1)).toBe(1);
     });
 });
