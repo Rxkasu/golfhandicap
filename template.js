0a47f2cbb2d3343c941d3780617247395eb321d8
Philipp Arnold <20288996+SkyllarRisen@users.noreply.github.com>
2025-01-27 18:27:46 +0100

    feat: added vitest testing framework setup and a example/template for writing tests

---
+++ template.js
@@ -0,0 +1,4 @@
+// Example for testing a function that returns the greater of two numbers
+export function template(a, b) {
+  return (a > b) ? a : b;
+}
---
+++ tests/template.test.js
@@ -0,0 +1,22 @@
+import { describe, test, it, expect } from "vitest";
+import { template } from "../template";
+
+describe('TEMPLATE', () => {
+    it('TEST_NAME_1', () => {
+        // Pattern: Arrange, Act, Assert.
+        // Arrange:
+        const a = 1;
+        const b = -2;
+
+        // Act:
+        const res = template(a, b);
+
+        // Assert:
+        expect(res).toBe(1);
+        expect(res).not.toBe(-2);
+    });
+    it('TEST_NAME_2', () => {
+        // Alternative:
+        expect(template(1, -2)).toBe(1);
+    });
+});
