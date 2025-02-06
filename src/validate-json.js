22c5fde0233fe05e806913b3e8d5335b876b7145
Philipp Arnold <20288996+SkyllarRisen@users.noreply.github.com>
2025-02-06 17:41:41 +0100

    feat: Added functionality for validating JSON objects against defined schema

---
+++ src/validate-json.js
@@ -0,0 +1,27 @@
+
+
+function validateJson(json) {
+
+    const Ajv = require('ajv');
+    const ajv = new Ajv({allErrors: true});
+
+    const schema = require('./json/schema/json-schema.json');
+   
+    if(!ajv.validateSchema(schema)) {
+        console.error('JSON Schema not valid!\nValidation Errors:', ajv.errorsText());
+        throw new Error('JSON Schema not valid!');
+    }
+
+    if(!ajv.validate(schema, json)) {
+        console.error('JSON not valid!\nValidation errors:', ajv.errorsText());
+        return false;
+    } else {
+        console.log('JSON data is valid!');
+    }
+
+    return true;
+}
+
+const data = require('../tests/data/test-data.json')
+
+validateJson(data);
